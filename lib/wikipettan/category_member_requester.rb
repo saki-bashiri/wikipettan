63339ee00d46e26854af348d2a9937a7b7c1ab0b
saki-bashiri <mov.an.double@gmail.com>
2016-08-27 13:39:14 +0900

    switch Category#member_ids, Category#member_names to Categry#members

--- lib/wikipettan/category_member_requester.rb
+++ lib/wikipettan/category_member_requester.rb
@@ -1,9 +1,12 @@
 require 'wikipettan/requester'
+require 'wikipettan/category_member'
 module Wikipettan
   class CategoryMemberRequester < Wikipettan::Requester
-    def member_hashes
-      return [] unless json["query"]
-      json["query"]["categorymembers"]
+    def members
+      return [] if !json.key?("query") || !json["query"].key?("categorymembers")
+      json["query"]["categorymembers"].map do |member|
+        Wikipettan::CategoryMember.new(member["pageid"], title: member["title"])
+      end
     end
 
     def continue?
