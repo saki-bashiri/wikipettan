4dda4a1e5cc1ba127f8ea06307e1debd88bb55b6
saki-bashiri <mov.an.double@gmail.com>
2016-08-27 13:13:44 +0900

    fix typo error

--- lib/wikipettan.rb
+++ lib/wikipettan.rb
@@ -7,7 +7,7 @@ require 'wikipettan/category'
 module Wikipettan
   class << self
     def category(pageid: nil, title: nil)
-      Wikipettan::Category.new(ageid: pageid, title: title)
+      Wikipettan::Category.new(pageid: pageid, title: title)
     end
   end
 end
